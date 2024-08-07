a37f994d85c61465f3606ad2ba9d6d53e5ca9521
Karthikeyanl77 <karthisachin50@gmail.com>
2024-08-07 09:12:02 +0530

    Initial Commit

---
+++ SeperateCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeperateCalculator
+//
+//  Created by Karthikeyan.L on 07/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
